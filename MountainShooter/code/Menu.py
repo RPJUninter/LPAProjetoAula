4f73efdc05cce4ea1cdb9a35ee7371b49a5f22d5
RenanJorge <renan.j@uninter.com>
2023-08-10 20:08:21 -0300

    Aula sobre criação e movimento de entidades

--- MountainShooter/code/Menu.py
+++ MountainShooter/code/Menu.py
@@ -19,6 +19,7 @@ class Menu:
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
             self.menu_text(50, "Mountain", (255, 128, 0), ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Shooter", (255, 128, 0), ((WIN_WIDTH / 2), 70))
             pygame.display.flip()
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
